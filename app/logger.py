92f0d7c4c89548138bfcd92c579fa23e351e5054
Андрей <elkabor@mail.ru>
2025-04-06 21:01:41 +0300

    fixed structure

---
+++ app/logger.py
@@ -0,0 +1,19 @@
+import logging
+
+
+def setup_logger():
+    logger = logging.getLogger("app")
+    logger.setLevel(logging.DEBUG)
+
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
+    return logger
+
+
+logger = setup_logger()
